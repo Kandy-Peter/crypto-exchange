4675f80db1a704a2769c20f5f87e5ee804515e7d
Kandy Peter <kandypeter03@gmail.com>
2022-03-15 13:09:56 +0200

    add style to the details page

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -26,17 +26,46 @@ const CryptoDatails = () => {
             </Link>
             <p>{detail.symbol}</p>
           </div>
-          <div className="main-clm">
-            <img src={Image(detail.symbol)} alt="crypto-img" />
+          <div
+            className="main-clm"
+            style={{ '--img': `url(${Image(detail.symbol)})` }}
+          >
             <h1>{detail.symbol}</h1>
           </div>
-          <span>CRYPTO DETAILS:</span>
+          <div className="descr"><p>CRYPTO DETAILS:</p></div>
           <ul className="coin-cards">
-            <li className="card">{detail.priceChangePercent}</li>
-            <li className="card">{detail.lastPrice}</li>
-            <li className="card">{detail.volume}</li>
-            <li className="card">{detail.highPrice}</li>
-            <li className="card">{detail.lowPrice}</li>
+            <li className="card">
+              <p>Price Change Percent:</p>
+              <p>{detail.priceChangePercent}</p>
+            </li>
+            <li className="card">
+              <p>Last Price:</p>
+              <p>{detail.lastPrice}</p>
+            </li>
+            <li className="card">
+              <p>Volume:</p>
+              <p>{detail.volume}</p>
+            </li>
+            <li className="card">
+              <p>High Price</p>
+              <p>{detail.highPrice}</p>
+            </li>
+            <li className="card">
+              <p>Low Price:</p>
+              <p>{detail.lowPrice}</p>
+            </li>
+            <li className="card">
+              <p>Price Change:</p>
+              <p>{detail.priceChange}</p>
+            </li>
+            <li className="card">
+              <p>Open Price:</p>
+              <p>{detail.openPrice}</p>
+            </li>
+            <li className="card">
+              <p>Count:</p>
+              <p>{detail.count}</p>
+            </li>
           </ul>
         </div>
       ))}
